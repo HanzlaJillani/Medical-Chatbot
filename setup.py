46676bdeadf43f29f33d97e2b554c46dd327b6a7
HanzlaJillani <hanzlajillani10@gmail.com>
2025-08-27 16:18:23 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='Medical-Chatbot',
+    version='0.1.0',
+    author='Hanzla Jillani',
+    author_email='hanzlajillani10@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
